2ee21887ee5c8d52caff612e04d940491ce79d21
neilbhutada <50277952+neilbhutada@users.noreply.github.com>
2020-10-16 06:31:47 +0530

    Update Record.java

--- Data Wrangling/Record.java
+++ Data Wrangling/Record.java
@@ -1,19 +1,27 @@
 /**
- * The Record class will store all the values that have to be stored in the RBT. 
+ * The Record class will store all the values that have to be stored in the RBT.
  * The Back-end will just the Record object in his code
  * 
  *
  */
-public class Record
-{
-	int number; 
+public class Record implements Comparable<Record> {
+	int number;
 	String description;
-	Record(int n, String d)
-	{
-		number = n;  //Course number 
-		description = d; //Course description
-		
+
+	Record(int n, String d) {
+		number = n; // Course number
+		description = d; // Course description
+
 	}
 
+	@Override
+	public int compareTo(Record data) {
+		if (this.number > data.number)
+			return 1;
+		else if (this.number == data.number)
+			return 0;
+		else
+			return -1;
+	}
 
 }
