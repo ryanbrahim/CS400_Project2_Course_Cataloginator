f46a24d86300cc694f3538018ca7ee47a0c29216
neilbhutada <50277952+neilbhutada@users.noreply.github.com>
2020-10-16 05:48:18 +0530

    Update Wrangle.java

--- Data Wrangling/Wrangle.java
+++ Data Wrangling/Wrangle.java
@@ -1,51 +1,51 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 /**
  * A demo way to use this class in a function would be: 
- * Wrangle w = new
- * Wrangle("Final.csv"); 
- * List <Record> a = w.load(); for (Record record: a) 
- * {
- * System.out.println(record.number+" -- "+record.description); 
+ * Wrangle w = new Wrangle("Final.csv"); 
+ * while(w.input.hasNext()) 
+ * { 
+ * //insert in Red Black Tree by calling w.nextCourse()
+ * //w.nextCourse() will return record object.
  * }
- * 
  */
 public class Wrangle {
 
 	String filename;
 	File file;
 	List<Record> records;
+	Scanner input;
+	String data;
 
-	Wrangle(String filename) {
+	Wrangle(String filename) throws FileNotFoundException {
 		this.filename = filename;
 		file = new File(filename);
 		records = new ArrayList<Record>();
+		input = new Scanner(file);
+		data = input.next(); // Avoids header column
 	}
 
 	/**
 	 * The load function extracts data from the csv file.
 	 * 
-	 * @return a list of records
+	 * @return Record object
 	 */
-	public List<Record> load() {
+	public Record nextCourse() {
 
-		int flag = 0;
+		Record temp;
 		try {
-			Scanner input = new Scanner(file);
-			String data = input.next();
-			while (input.hasNext()) {
-				Record temp;
-				data = input.next();
-				String[] cols = data.split(",");
-				temp = new Record(Integer.parseInt(cols[0]), cols[1]); // Instantiating records object
-				records.add(temp);
-			}
+			data = input.next();
+			String[] cols = data.split(",");
+			temp = new Record(Integer.parseInt(cols[0]), cols[1]); // Instantiating records object
+			return temp;
 
 		} catch (Exception e) {
 			e.printStackTrace();
+			return null;
 		}
-		return records;
+
 	}
 
 }
